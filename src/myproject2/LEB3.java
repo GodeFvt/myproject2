5b9f1a094d03d1f7b29cd28e5f0246009ab1ab0d
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2023-03-31 23:08:04 +0700

    Add files via upload

---
+++ src/myproject2/LEB3.java
@@ -0,0 +1,19 @@
+package myproject2;
+
+public class LEB3 {
+    public static void main(String[] args) {
+        int[] list = new int[10];
+        try{
+            System.out.println("Statement 1");
+            System.out.println(list[10]);
+            System.out.println("Statement 3");
+        }
+        catch (ArithmeticException ex){
+            System.out.println("Exception");
+        }
+        finally{
+            System.out.println("Statement 4");
+        }
+        System.out.println("Statement 5");
+    }
+}
---
+++ src/myproject2/MyException.java
@@ -0,0 +1,10 @@
+package myproject2;
+
+public class MyException extends Exception {
+    public MyException() {
+        super();
+    }
+    public MyException(String msg) {
+        super(msg); // must call parents class
+    }
+}
